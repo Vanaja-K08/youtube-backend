5e224bd7377e52f7c1936596e4d499e3cef88d91
vanaja <vanajakoppad@gmail.com>
2026-02-07 21:32:39 +0530

    created authontication and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -14,4 +15,25 @@ export const register = async (req, res) => {
 
   await user.save();
   res.status(201).json({ message: "User registered" });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid password" });
+  }
+
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token, user });
 };
